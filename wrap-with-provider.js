8e6972a98931508da514293b311af9f9a4667c7d
Michael Auderer <mike@silverstone.io>
2020-08-03 01:47:07 -0400

    fix: favicon and global css

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { QuantumProvider } from 'quantum-elements'
 import theme from './src/styles/theme'
+import './src/styles/global.css'
 
 export default ({ element }) => (
   <QuantumProvider theme={theme}>
